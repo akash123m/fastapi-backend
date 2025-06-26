bcd64fc26e952af3c0384a6c289a4ce1a29b2ea8
Akash Maity <73158127+akash123m@users.noreply.github.com>
2025-06-27 01:02:52 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,7 +2,8 @@
 
 # Force uninstall the broken dummy 'google' package
 pip uninstall -y google || true
-
+pip uninstall google google-api-python-client google-cloud -y
+pip install google-generativeai
 # Install everything cleanly and explicitly
 pip install --no-cache-dir fastapi uvicorn python-dotenv google-generativeai==0.3.2
 
